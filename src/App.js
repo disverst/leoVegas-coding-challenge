a43ac29c22bd3b15db37c3d9e34ab1bd069d9b4c
disverst <disverst@gmail.com>
2023-05-11 10:00:26 +0200

    1. Added the Modal component inside the container div, passing the isOpen, closeModal, and videoKey props. 2. Removed the conditional rendering of YouTubePlayer and replaced it with the Modal component. Now, the video player will be rendered inside the modal when videoKey is available.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
-import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
 import { useDispatch, useSelector } from 'react-redux'
+import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
 import 'reactjs-popup/dist/index.css'
 import { fetchMovies } from './data/moviesSlice'
 import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants'
 import Header from './components/Header'
+import Modal from './components/Modal';
 import Movies from './components/Movies'
 import Starred from './components/Starred'
 import WatchLater from './components/WatchLater'
-import YouTubePlayer from './components/YoutubePlayer'
 import './app.scss'
 
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
   const dispatch = useDispatch()
   const [searchParams, setSearchParams] = useSearchParams()
   const searchQuery = searchParams.get('search')
-  const [videoKey, setVideoKey] = useState()
+  const [videoKey, setVideoKey] = useState(null)
   const [isOpen, setOpen] = useState(false)
   const navigate = useNavigate()
   
@@ -79,14 +79,7 @@ const App = () => {
       <Header searchMovies={searchMovies} searchParams={searchParams} setSearchParams={setSearchParams} />
 
       <div className="container">
-        {videoKey ? (
-          <YouTubePlayer
-            videoKey={videoKey}
-          />
-        ) : (
-          <div style={{padding: "30px"}}><h6>no trailer available. Try another movie</h6></div>
-        )}
-
+        <Modal isOpen={isOpen} closeModal={closeModal} videoKey={videoKey} />
         <Routes>
           <Route path="/" element={<Movies movies={movies} viewTrailer={viewTrailer} closeCard={closeCard} />} />
           <Route path="/starred" element={<Starred viewTrailer={viewTrailer} />} />
